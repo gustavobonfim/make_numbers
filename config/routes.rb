b65c14635c5a3b8fdb384360ffe8502a19552810
gustavobonfim <bonfimoliveira@gmail.com>
2017-11-16 11:46:54 -0200

    fixed some bugs and implement error pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get '/error_page_controller', to: 'error_page_controller#pages'
 
+  match "/404", to: 'errors#not_found', via: :all
+  match "/422", to: 'errors#unnacceptable', via: :all
+  match "/500", to: 'errors#internal_error', via: :all
+
   namespace :make_numbers do
     get 'cnpj'
     get 'cpf'
